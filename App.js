d7e0176667523d8e2c4cebf3007d7478b5061740
Mohammad Pasha <mdpasha37@gmail.com>
2021-03-27 10:48:50 +0530

    complete listing edit screen

--- App.js
+++ App.js
@@ -52,23 +52,9 @@ const categories = [
   },
 ];
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
   return (
     <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      />
+      <ListingEditScreen></ListingEditScreen>
     </Screen>
   );
 }
